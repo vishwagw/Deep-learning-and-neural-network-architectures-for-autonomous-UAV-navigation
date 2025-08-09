9caa223302b607c5ec58c5f79434a9ee2068af3b
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-08-09 14:42:01 +0530

    Add files via upload

---
+++ CNN-based approach for autonomous navigation/Quality_control/optical_flow_cal.py
@@ -0,0 +1,5 @@
+def optical_flow(prev_frame, next_frame):
+    prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+    next_gray = cv2.cvtColor(next_frame, cv2.COLOR_BGR2GRAY)
+    flow = cv2.calcOpticalFlowFarneback(prev_gray, next_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    return flow
